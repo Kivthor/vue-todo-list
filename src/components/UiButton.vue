d895f59458092cade67af6361c1c32be3240cc1a
Kivthor <50101494+Kivthor@users.noreply.github.com>
2024-12-17 18:24:53 +0700

    what

---
+++ src/components/UiButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from "vue";
+
+const emit = defineEmits(["action"]);
+const props = defineProps({
+  buttonType: String,
+});
+
+const isItDeleteButton = computed(() => props.buttonType === "DELETE");
+const onClick = () => {
+  emit("action");
+  // console.log("GAVNO");
+};
+</script>
+
+<template>
+  <button @click="onClick" type="button" :class="{ red: isItDeleteButton }">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.block {
+  display: block;
+}
+
+.red {
+  background: red;
+  color: white;
+}
+</style>
---
+++ src/components/UiInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input :type="type" :name="name" class="todo-item-name-input" :placeholder="placeholder" :maxlength="maxlength"
+    :model-value="modelValue" @input="onInput" />
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: String,
+  type: String,
+  name: String,
+  placeholder: String,
+  maxlength: Number,
+});
+
+const emit = defineEmits(["update:model-value"]);
+
+const onInput = (event) => {
+  emit("update:model-value", event.target.value);
+};
+</script>
+
+<style scoped></style>
